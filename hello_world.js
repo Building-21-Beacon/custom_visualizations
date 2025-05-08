cc39d9d752c327dbb3240cafe315a0756523a230
eliana-b21 <139242237+eliana-b21@users.noreply.github.com>
2025-05-08 10:18:38 -0400

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -19,31 +19,49 @@ looker.plugins.visualizations.add({
     }
   },
   create: function(element, config) {
+    this.addScript("https://d3js.org/d3.v5.min.js")
+    .then(() => {
+      console.log("D3 loaded:", d3.version);
     element.innerHTML = `
       <style>
-        .aster-tooltip {
-          position: absolute;
-          text-align: center;
-          padding: 6px;
-          font: 12px sans-serif;
-          background: lightsteelblue;
-          border: 0px;
-          border-radius: 4px;
-          pointer-events: none;
-          opacity: 0;
-        }
-      </style>
-    `;
-    this._svg = d3.select(element)
-      .append("svg")
-      .style("width", "100%")
-      .style("height", "100%");
+          .aster-plot {
+            width: 100%;
+            height: 100%;
+          }
+          .tooltip {
+            position: absolute;
+            text-align: center;
+            padding: 6px;
+            font: 12px sans-serif;
+            background: lightsteelblue;
+            border: 0px;
+            border-radius: 8px;
+            pointer-events: none;
+            opacity: 0;
+          }
+        </style>
+      `;
+    this._container = d3.select(element)
+        .append("div")
+        .attr("class", "aster-plot");
 
-    this._tooltip = d3.select(element)
-      .append("div")
-      .attr("class", "aster-tooltip");
+      this._svg = d3.select(element).select(".aster-plot")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .style("min-height", "400px");  // Important: ensure visible size
+
+      // Tooltip
+      this._tooltip = d3.select(element).append("div")
+        .attr("class", "tooltip");
+    });
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
+    if (typeof d3 === "undefined") {
+      console.warn("D3 not yet loaded, skipping render");
+      done();
+      return;
+    }
     this.clearErrors();
 
     if (queryResponse.fields.dimensions.length === 0 || queryResponse.fields.measures.length === 0) {
@@ -99,12 +117,12 @@ looker.plugins.visualizations.add({
     slices.append("path")
       .attr("d", arc)
       .attr("fill", color)
-      .on("mouseover", function(event, d) {
+      .on("mouseover", function(d) {
         tooltip
           .style("opacity", 1)
           .html(`<strong>${d.data.label}</strong><br>${d.data.value}%`)
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
       })
       .on("mouseout", function() {
         tooltip.style("opacity", 0);
